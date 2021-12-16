d0d179afd05eaf10171ed753f2f00bd87618622e
Andres Molina <lancedres@gmail.com>
2021-12-16 10:51:59 +0100

    deploy try 3

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ const app = express();
 
 require("./config")(app);
 require("./config/session.config")(app)
+app.use(express.static(path.join(__dirname, "public")))
 
 require("./routes")(app);
 
 require("./error-handling")(app);
 
-app.use(express.static(path.join(__dirname, "public")))
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 module.exports = app;
